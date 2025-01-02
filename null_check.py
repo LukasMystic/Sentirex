70d759677b495ef861d1a7becb0f0dc8b386bb5d
LukasMystic <112644367+LukasMystic@users.noreply.github.com>
2025-01-02 11:27:43 +0700

    Update null_check.py

--- null_check.py
+++ null_check.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-# Load your dataset from an Excel file
+
 df = pd.read_excel('preprocessed.xlsx')
 
-# Check for any null values in the 'sentimen' column
+
 null_values = df[df['sentimen'].isnull()]
 
 if not null_values.empty:
